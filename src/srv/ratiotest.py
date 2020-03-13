f8f5a8168c4bc28a5b92f535e2a55f9bc3271158
luke <lukeg496@gmail.com>
2020-03-13 11:40:12 -0500

    Update to make flask - also rework some comments

--- src/srv/ratiotest.py
+++ src/srv/ratiotest.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-#import math
 import sympy
 from sympy import sympify
 from sympy import *
-#import sys
-#from flask_cors import CORS
-#
-#from flask import Flask, request, jsonify
-#
-#app = Flask(__name__)
-#CORS(app)
+import sys
+from flask_cors import CORS
 
-#n= symbols('n', integer=True)
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+CORS(app)
+
 n= symbols('n')
 
 
@@ -19,26 +17,24 @@ n= symbols('n')
 ops=['+','-','*','/','(',')']#,'^','!'] # let's start with these also, ** is exponentiation
 nums=['1','2','3','4','5','6','7','8','9','0','.']
 var='n'
-#par=['(',')']
 words=['factorial','sin','cos','exp','ln']# 
 
 def filter(summand):
     '''
-    checks that there are only acceptable characters
-    returns indices of first unnacceptable word or
-    returns True if they're all good
+    Checks that there are only acceptable characters
+    Parameters:
+        summand (string): The summand to check
+    Returns:
+        err (string): The first unnacceptable character found
+        True: if all acceptable characters
     '''
     sx = summand+' '
-#    if 'x' in sx:
-#        return 'x'
-#    else:
-    '''
-    changes all the acceptable characters to ' '
-    the order is important. Like if you deleted the 'n's first, 
-    then 'sin' would be 'si ' which is not acceptable.
-    also, if you just deleted the acceptable characters, 
-    then 'csinosins' would be acceptable.
-    '''
+    # changes all the acceptable characters to ' '
+    # the order is important. Like if you deleted the 'n's first, 
+    # then 'sin' would be 'si ' which is not acceptable.
+    # also, if you just deleted the acceptable characters, 
+    # then 'csinosins' would be acceptable.
+
     for s in words:
         if s in sx:
             sx=sx.replace(s,' ')
@@ -64,16 +60,16 @@ def filter(summand):
     return True
 
 
-
-
-def ratio():
+def ratio(summand):
     '''
-    first entry
-        0         - it calculated a limit, limit is the second entry
-        1         - math typo
-        2         - unnacceptable input
+    Flask-available function to calculate ratio test
+    Parameters:
+        summand (string): a summand to calculate the ratio test of
+    Returns:
+        list [int, message (string)]: A number to signify error type, and the error
+        list [0, res(float)]: 0 to signify no error, and the result of the calculation
     '''
-    seq=input('Enter summand: ')
+    seq=summand
     f=filter(seq)
     if f is True:
         try:
@@ -86,37 +82,42 @@ def ratio():
         else:
             # computes the ratio and limit
             res=limit(seq/seq.subs(n,n-1),n,oo)
-            if res == 1:
-                return [0,1]
-            elif res > 1:
-                return [0,res]
-            else:
-                return[0,res]
+            return[0,res]
     else:
         errorMessage = '"'+f+'"'+' is not an acceptable word/character'
         return [2,errorMessage]
 
-print(ratio())
-#@app.route('/ratioTest', methods = ['POST', 'GET'])
-#def run():
-#        print(request.args['summand'], file=sys.stdout)
-#        global ss
-#        ss = get(request.args['summand'])
-#        print(ss, file=sys.stdout)
-#        res = ratio()
-#        message = ''
-#
-#        if res[0] == 0:
-#                message = "Test inconclusive"
-#        elif res[0] == -1:
-#                message = "Series probably converges"
-#        elif res[0] == 1:
-#                message = "Series probably diverges"
-#        return jsonify(result = message,
-#                       num = float(res[1])
-#        )
-#
-#
-#if __name__ == '__main__':
-#    app.run(host = '0.0.0.0', port=8000)
-#
+
+@app.route('/ratioTest', methods=['POST', 'GET'])
+def run():
+    '''
+    A flask endpoint to trigger ratio test calculation
+    Expected payload:
+        {
+            summand: summand
+        }
+    '''
+    print(request.args['summand'], file=sys.stdout)
+    ss = request.args['summand']
+    print(ss, file=sys.stdout)
+    res = ratio(ss)
+    message = ''
+    if res[0] == 0: 
+        if res[1] == 1:
+            message = "Test inconclusive"
+        elif res[1] < 1 :
+            message = "Converges"
+        elif res[1] > 1:
+            message = "Diverges"
+        return jsonify(result = message,
+                       num = float(res[1])
+        )
+    elif res[0] == 1 or res[0] == 2:
+        return jsonify(result = res[1],
+                       num = 'Error:'
+        )
+
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port=8000)
+
